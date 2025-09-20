075b9e08b0b893288c26e0e8e0990c5d4da2fde8
Pierre-Alexandre35 <46579114+Pierre-Alexandre35@users.noreply.github.com>
2025-09-20 11:54:52 +0200

    wip

--- migrations/001_init.up.sql
+++ migrations/001_init.up.sql
@@ -1,3 +1,6 @@
+-- needed for CITEXT columns
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE IF NOT EXISTS users (
   id            BIGSERIAL PRIMARY KEY,
   email         CITEXT UNIQUE NOT NULL,
